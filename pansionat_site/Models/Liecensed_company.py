26fb37c836e8be57e1926a03abf606173e2e1786
OsmanGungor <98587536+OsmanGungor@users.noreply.github.com>
2024-11-20 11:57:33 +0300

    Delete pansionat_site directory

--- pansionat_site/Models/Liecensed_company.py
+++
@@ -1,9 +0,0 @@
-class LicensedCompany:
-    def __init__(self, id, generated_number, activity_id, activity_code, activity_name, unp, holder_name):
-        self.id = id
-        self.generated_number = generated_number
-        self.activity_id = activity_id
-        self.activity_code = activity_code
-        self.activity_name = activity_name
-        self.unp = unp
-        self.holder_name = holder_name
